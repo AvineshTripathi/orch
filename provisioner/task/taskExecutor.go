2cc969c715aa68c6736b57f6e5e713c142df0010
AvineshTripathi <avineshtripathi1@gmail.com>
2024-12-08 23:20:33 +0530

    feat: added multiple plugin load support

--- provisioner/task/taskExecutor.go
+++ provisioner/task/taskExecutor.go
@@ -1,37 +1,58 @@
-// Package task demonstrates how to create a modular Go program using plugins.
-// It defines the Task interface and TaskExecutor struct, providing methods for loading and executing plugins.
 package task
 
 import (
 	"fmt"
+	"os"
 	"plugin"
 	"sync"
+
+	"gopkg.in/yaml.v2"
 )
 
 type TaskExecutor struct {
 	mu    sync.Mutex
-	tasks []*plugin.Plugin
+	plugins map[string]*plugin.Plugin
 }
 
 func NewTaskExecutor() *TaskExecutor {
-	return &TaskExecutor{}
+	return &TaskExecutor{
+		plugins: make(map[string]*plugin.Plugin),
+	}
 }
 
 func (t *TaskExecutor) LoadPlugin(path string) error {
-	p, err := plugin.Open(path)
+
+	file, err := os.ReadFile(path)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to read YAML file: %v", err)
 	}
-	t.tasks = append(t.tasks, p)
+
+	var pluginConfig map[string]string
+	err = yaml.Unmarshal(file, &pluginConfig)
+	if err != nil {
+		return fmt.Errorf("failed to parse YAML file: %v", err)
+	}
+
+	t.mu.Lock()
+	defer t.mu.Unlock()
+
+	for functionName, pluginPath := range pluginConfig {
+		p, err := plugin.Open(pluginPath)
+		if err != nil {
+			return fmt.Errorf("failed to load plugin '%s' from path '%s': %v", functionName, pluginPath, err)
+		}
+		t.plugins[functionName] = p
+	}
 	return nil
 }
 
 func (t *TaskExecutor) Execute(task *Task) error {
 	t.mu.Lock()
 	defer t.mu.Unlock()
-	for _, p := range t.tasks {
+	  
+	for _, p := range t.plugins {
 
-		runFunc, err := p.Lookup("Run")
+		runFunc, err := p.Lookup(task.Plugin)
 		if err != nil {
 			return err
 		}
@@ -50,7 +71,7 @@ func (t *TaskExecutor) Execute(task *Task) error {
 
 		err = fn(task.Config)
 		if err != nil {
-			fmt.Errorf("Error while running the plugin: %v", err)
+			return fmt.Errorf("error while running the plugin: %v", err)
 		}
 	}
 
